b9c8569cda3a47f2d176a1339da5369d179b7c65
Liza Cummings <lizacummings@utexas.edu>
2020-01-03 19:12:01 +0000

    permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,30 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
   def show
   end
 
   private
 
-  helper_method :current_lesson
+  def require_user_be_enrolled
+    if !current_user.enrolled_in?(current_lesson.section.course)
+        redirect_to course_path(current_lesson.section.course), alert: 'You are not enrolled in this course. You must enroll before you can view the lessons!'
+    end
+  end
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+   helper_method :current_lesson
   def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+    @current_lesson ||= Lesson.find(params[:section_id])
+  end
+
+def lesson_params
+    params.require(:lesson).permit(:title, :subtitle, :video)
   end
 end
+
