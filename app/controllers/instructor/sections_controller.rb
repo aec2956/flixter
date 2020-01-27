e4155469525182dca7cd901263cc8c125957bdfe
Liza Cummings <lizacummings@utexas.edu>
2020-01-27 16:48:29 +0000

    lesson 35

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,11 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:create]
   before_action :require_authorized_for_current_section, only: [:update]
-
-  def new
-    @section = Section.new
-  end
 
   def create
     @course = Course.find(params[:course_id])
